035e7173efca6048060dad5a2bba1c057c798ff9
hiraBiswas <hbhirabiswas@gmail.com>
2023-09-11 22:42:05 +0600

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-6xl font-bold bg-red-200'>Knowladge Cafe</h1>
+    </>
+  )
+}
+
+export default App
